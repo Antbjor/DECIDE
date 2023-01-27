7d6e3d90ffcf58096aed177453e5f89b3e258157
wenqic <wenqic@kth.se>
2023-01-27 23:27:39 +0100

    fix: Add parameter test for code-base and update test skeleton
    fixes issue #24

--- tests/base_code_test.py
+++ tests/base_code_test.py
@@ -2,7 +2,6 @@ import yaml
 import config
 import unittest
 
-
 try:
     with open('settings.yml', encoding='utf-8') as fin:
         test_data = yaml.load(fin, Loader=yaml.FullLoader)
@@ -12,11 +11,11 @@ except FileNotFoundError:
 
 
 class TestInputData(unittest.TestCase):
-
     num_points = 100  # Remember to change this parameter
 
     def test_num_points(self):
         self.assertEqual(config.num_points, self.num_points)
+        self.assertTrue(2 <= config.num_points <= 100, "ERROR: invalid NUMPOINTS.")
 
     def test_point_x(self):
         point_x = test_data["POINTS_X"]
@@ -41,6 +40,12 @@ class TestInputData(unittest.TestCase):
                "true", "true", "true", "true", "true"]
         self.assertEqual(config.puv, puv)
         self.assertEqual(config.puv, test_data["PUV"])
+
+    def test_parameters(self):
+        param = config.parameters
+        self.assertTrue(0 <= param["AREA1"], "ERROR: AREA1 < 0 in settings.")
+        self.assertTrue(1 <= param["K_PTS"] <= param["NUMPOINTS"] - 2, "ERROR: invalid K_PTS in settings.")
+        self.assertTrue(1 <= param["G_PTS"] <= param["NUMPOINTS"] - 2, "ERROR: invalid G_PTS in settings.")
 
 
 if __name__ == '__main__':
