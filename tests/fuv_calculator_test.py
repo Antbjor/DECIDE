88017c75ab7828ae505eab4637928fd1dec5beb1
wenqic <wenqic@kth.se>
2023-01-31 14:23:47 +0100

    doc: add comments for fuv tests (#23)

--- tests/fuv_calculator_test.py
+++ tests/fuv_calculator_test.py
@@ -1,23 +1,32 @@
 import config
 import unittest
-import computations
 import decide
-import math
 
 decide.cmv_calculator(config.parameters, config.num_points, config.X, config.Y)
 decide.pum_calculator(config.cmv, config.lcm)
 decide.fuv_calculator(config.puv, config.pum)
 
+
 class TestFuvFunction(unittest.TestCase):
     
     def test_fuv_size(self):
+        """
+        test_fuv_size tests if the size of puv vector is the same as fuv vector.
+
+        expected_outcome: True
+        """
         self.assertEqual(len(config.puv), len(config.fuv), "ERROR: size difference.")
-        
-    
+
     def test_fuv_true_puv_false(self):
+        """
+        test_fuv_true_puv_false tests the situation that fuv vector is true but puv is false.
+
+        expected_outcome: True
+        """
         for i in range(len(config.fuv)):
             if not(config.puv[i]):
                 self.assertTrue(config.fuv[i])
-    
+
+
 if __name__ == '__main__':  
     unittest.main()
